303242c73f41845f5eaafb09222abd46745c1447
stokes <51892+adam-stokes@users.noreply.github.com>
2026-04-21 21:59:34 -0400

    fix(site): gitignore shadow-cljs build artifacts

--- site/resources/public/js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { gl1tch.site.core.init(); } catch (e) { console.error("An error occurred when calling (gl1tch.site.core/init)"); console.error(e); }
